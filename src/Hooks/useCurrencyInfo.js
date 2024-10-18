0c1144488e4b6204588afe9f277fc1cf68c8f13d
YuvrajGill01 <yuvrajsinghg213@gmail.com>
2024-10-18 15:12:30 +0530

    optimized

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -13,7 +13,9 @@ function useCurrencyInfo(currency)
             .then((res) => setData(res[currency]))
             console.log(data)
     },[currency])
-    return data
+    return data;
 }
 
 export default useCurrencyInfo;
+
+//https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json
